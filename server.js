8710896492c6d51ff15b7e22e369af5ed4da2fe6
Felipe Acosta <facosta0787@gmail.com>
2018-12-20 16:15:48 -0500

    minor change

--- server.js
+++ server.js
@@ -5,5 +5,5 @@ app.listen(3000, (error) => {
     console.log(error)
     process.exit(1)
 }
-console.log(`[app] Server is running and listening on http://localhost:3000`)
+console.log(`[app-log] Server is running and listening on http://localhost:3000`)
 })
